bb21a16c728e2a7bde3b860fb7b46052354b23c9
prashantbokade08 <prashantbokade08@gmail.com>
2024-08-15 20:23:40 +0530

    Create agentinstall.sh

---
+++ agentinstall.sh
@@ -0,0 +1,7 @@
+sudo yum update -y
+sudo yum install ruby wget -y
+cd /home/ec2-user
+wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
